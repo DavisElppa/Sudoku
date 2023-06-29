d5fe96306a2eb80050428fdc7c18aa40ed8dfb16
DavisElppa <3279087558@qq.com>
2023-06-29 10:36:03 +0800

    merge Play_game to sudoku.cpp

--- sudoku.cpp
+++ sudoku.cpp
@@ -251,14 +251,207 @@ void Generate_NewGame(const string& input_path, const string& output_path, int n
     fout.close();
 }
 
+//***************************************************************************
+//***************************************************************************
+//******************************求解数独游戏部分******************************
+//***************************************************************************
+//***************************************************************************
+
+int a[11][11];//存储数独游戏
+int b[11];//每行已知数最多
+int bid[11];//b的id
+int bbid;//bid第几个
+int c[11];//每列已知数最多
+int cid[11];//c的id
+int ccid;//cid第几个
+int alltheanswer;//所有解
+int wanttheanswer=1;
+bool checkx(int x) {//x为第几行
+	for (int i = 1; i <= 9; i++) {
+		if (a[x][i] == 0)
+			continue;
+		for (int j = 1; j < i; j++)
+			if (a[x][i] == a[x][j])
+				return false;
+	}
+	return true;
+}
+bool checky(int y) { //y为第几列
+	for (int i = 1; i <= 9; i++) {
+		if (a[i][y] == 0)
+			continue;
+		for (int j = 1; j < i; j++)
+			if (a[i][y] == a[j][y])
+				return false;
+	}
+	return true;
+}
+bool jggfz(int djgg) {
+	int x, y;
+	if (djgg == 1)
+		x = 1, y = 1;
+	else if (djgg == 2)
+		x = 1, y = 4;
+	else if (djgg == 3)
+		x = 1, y = 7;
+	else if (djgg == 4)
+		x = 4, y = 1;
+	else if (djgg == 5)
+		x = 4, y = 4;
+	else if (djgg == 6)
+		x = 4, y = 7;
+	else if (djgg == 7)
+		x = 7, y = 1;
+	else if (djgg == 8)
+		x = 7, y = 4;
+	else
+		x = 7, y = 7;
+	for (int k = 1; k <= 9; k++) {
+		bool f = false;
+		for (int i = x; i < x + 3; i++) {
+			for (int j = y; j < y + 3; j++) {
+				if (a[i][j] == 0)
+					continue;
+				if (a[i][j] == k && f == false)
+					f = true;
+				else if (a[i][j] == k)
+					return false;
+			}
+		}
+	}
+	return true;
+}
+void dfs(int na, int nb,const string& output_path) { //第几行第几个
+    if (alltheanswer >= 10000) //数量太过庞大，求解的速度就会变得很慢
+        return;
+	if (bbid == 10) {
+		alltheanswer++;
+		if (wanttheanswer >= alltheanswer) {
+			ofstream answer(output_path, ios::app);
+			if (answer.is_open()) {
+				for (int i = 1; i <= 9; i++) {
+					for (int j = 1; j <= 8; j++) {
+						//直接把第一个解写入文件answer.txt
+						//每次以追加的方式写入
+						answer << a[i][j] << " ";
+					}
+					answer << a[i][9] << endl;
+				}
+				answer.close();
+			}
+		}
+		return;
+	}
+	if (a[na][nb] != 0) {
+		int nna = na, nnb = nb, bbidd = bbid, ccidd = ccid;
+		if (ccid == 9)
+			bbid++, ccid = 1;
+		else
+			ccid++;
+		dfs(bid[bbid], cid[ccid],output_path);
+		na = nna, nb = nnb, bbid = bbidd, ccid = ccidd;
+		return;
+	}
+	for (int i = 1; i <= 9; i++) {
+		a[na][nb] = i;
+		int xx, yy;
+		int djgg = (na - 1) / 3 * 3 + (nb - 1) / 3 + 1;//第几个九宫格
+		if (checkx(na) == false || checky(nb) == false || jggfz(djgg) == false) {
+			a[na][nb] = 0;
+			continue;
+		}
+		int nna = na, nnb = nb, bbidd = bbid, ccidd = ccid;
+		if (ccid == 9)
+			bbid++, ccid = 1;
+		else
+			ccid++;
+		dfs(bid[bbid], cid[ccid],output_path);
+		na = nna, nb = nnb, bbid = bbidd, ccid = ccidd;
+		a[nna][nnb] = 0;
+	}
+}
+
+void solve_single_sudoku(const string& output_path)
+{
+	for (int i = 1; i < 10; i++)
+	{
+		for (int j = 1; j < 10; j++)
+		{
+			if (a[i][j] != 0)
+				b[i]++;
+			if (a[j][i] != 0)
+				c[i]++;
+		}
+	}
+
+	for (int i = 1; i <= 9; i++)
+		bid[i] = i, cid[i] = i;
+	for (int i = 1; i <= 9; i++) {
+		for (int j = 1; j <= 9 - i; j++) {
+			if (b[j] < b[j + 1])
+				swap(b[j], b[j + 1]), swap(bid[j], bid[j + 1]);
+			if (c[j] < c[j + 1])
+				swap(c[j], c[j + 1]), swap(cid[j], cid[j + 1]);
+		}
+	}
+
+	dfs(bid[++bbid], cid[++ccid],output_path); //直接求解所有答案,并写一个答案到txt中
+	ofstream answer(output_path, ios::app);
+	if (answer.is_open()){
+        if (alltheanswer < 10000)
+            answer << "number of answers : " << alltheanswer << endl << endl;
+        else
+            answer << "number of answers exceed 10000" << endl << endl;
+		answer.close();
+	}
+}
+
 void Play_Game(const string& input_path, const string& output_path) {
-    ifstream fin;
-    fin.open(input_path);
-    ofstream fout;
-    fout.open(output_path);
-    
-    fin.close();
-    fout.close();
+    ofstream answer(output_path, ios::trunc); //清空输出文件
+	answer.close();
+	ifstream game(input_path);  // 替换为您的文件名
+
+	//读取所有数独到一个二维数组内
+    int sudoku_num = 1;//统计完成数独的数量
+	int row = 1;
+	int col = 1;
+	if (game.is_open()){
+		string line;
+		while (getline(game, line)){
+			if (row != 0 && row % 10 == 0){
+				//说明读取完一个数独
+				//需要进行求解
+				//求解完后，各数组设置为初值
+				solve_single_sudoku(output_path);
+
+                cout << "求解完第"<<sudoku_num <<"个数独"<< endl;
+				memset(a, 0, sizeof(a));
+				memset(b, 0, sizeof(b));
+				memset(bid, 0, sizeof(bid));
+				bbid = 0;
+				memset(c, 0, sizeof(c));
+				memset(cid, 0, sizeof(cid));
+				ccid = 0;
+				alltheanswer = 0;
+				wanttheanswer = 1;
+				row = 1;
+                sudoku_num++;
+				continue;
+			}
+			for (char c : line){
+				if (c == '.'){
+					a[row][col] = 0;
+					col++;
+				}
+				else if (c >= '1' && c <= '9'){
+					a[row][col] = c - '0';
+					col++;
+				}
+			}
+			col = 1;
+			row++;
+		}
+	}
 }
 
 int main(int n_argc, char *argv[]) {
@@ -302,5 +495,6 @@ int main(int n_argc, char *argv[]) {
             }
         }
     }
+
     return 0;
 }
